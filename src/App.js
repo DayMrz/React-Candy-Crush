2b0a1408586fbe04b1fb574564a3c8fe2e47bc5e
Leily <leilymrz@gmail.com>
2024-03-22 20:35:34 -0400

    setting app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-function App() {
+import { useState } from "react"
+
+const width = 8
+const candyColors = [
+  'orange',
+  'blue',
+  'purple',
+  'green',
+  'red',
+  'yellow'
+]
+
+
+
+const App = () => {
+
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+  
+  const createBoard = () => {
+    const randomColorArrangement = []
+    for (let i = 0; i < width * width; i++) {
+      const randomColor =
+        candyColors[Math.floor(Math.random() * candyColors.length)]
+        randomColorArrangement.push(randomColor)
+    }
+    setCurrentColorArrangement(randomColorArrangement)
+  }
+  createBoard()
+
+  console.log(currentColorArrangement)
   return (
     <div> im empty </div>
   );
 }
 
-export default App;
+export default App
