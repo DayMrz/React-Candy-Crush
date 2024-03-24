77b469bececfefa5b25d7aff454894c7b4be97b1
Leily <leilymrz@gmail.com>
2024-03-24 13:30:21 -0400

    not valid numbers

--- src/App.js
+++ src/App.js
@@ -41,9 +41,13 @@ const App = () => {
   }
 
   const checkForRowOfFour = () => {
-    for (let i = 0; i < 39; i++) {
-      const rowOfFour = [i, i + 1, i + 1 * 2, i + 1 * 3]
+    for (let i = 0; i < 64; i++) {
+      const rowOfFour = [i, i + 1, i + 2, i + 3]
       const decidedColor = currentColorArrangement[i]
+      const notValid = 
+        [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+
+      if(notValid.includes(i)) continue
 
       if ( rowOfFour.every(square =>
         currentColorArrangement[square] === decidedColor)) {
@@ -54,9 +58,13 @@ const App = () => {
   }
 
   const checkForRowOfThree = () => {
-    for (let i = 0; i < 47; i++) {
+    for (let i = 0; i < 64; i++) {
       const rowOfThree = [i, i + 1, i + 1 * 2]
       const decidedColor = currentColorArrangement[i]
+      const notValid = 
+        [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
+
+      if(notValid.includes(i)) continue
 
       if ( rowOfThree.every(square =>
         currentColorArrangement[square] === decidedColor)) {
@@ -82,14 +90,14 @@ const App = () => {
   useEffect(() => {
     const timer = setInterval(() =>{
       checkForColumnOfFour()
-      checkForColumnOfThree()
       checkForRowOfFour()
+      checkForColumnOfThree()
       checkForRowOfThree()
       setCurrentColorArrangement([...currentColorArrangement])
     }, 100)
     return () => clearInterval(timer)
 
-  }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement])
+  }, [checkForColumnOfFour, checkForRowOfFour, checkForColumnOfThree,  checkForRowOfThree, currentColorArrangement])
 
   console.log(currentColorArrangement)
   return (
